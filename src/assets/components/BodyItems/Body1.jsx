abd5283d54215055d11f91c1ad13f1c44fe113f2
waikoo <b4rn1k4@yahoo.com>
2022-12-14 12:54:42 +0200

    Refactor Body1 to dynamically render inputs using data from radioInputs.jsx

--- src/assets/components/BodyItems/Body1.jsx
+++ src/assets/components/BodyItems/Body1.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { useRef } from 'react';
 import { SBody1 } from '../../css/styled';
+import { body1 } from '../../radioInputs';
 import InputLabel from '../Input';
 
 const Body1 = (props) => {
@@ -9,45 +10,32 @@ const Body1 = (props) => {
 
 	const [checked, setChecked] = useState('home');
 
-	const input = {
-		type: 'radio',
-		name: 'body1',
-		method: 'method',
-		homeRef: useRef(null),
-		storeRef: useRef(null),
-		value: { home: 'home', store: 'store' },
-		onChange: (e) => {
-			setChecked(e.target.value);
-			handleMethodChange(e);
-		}
+	const homeRef = useRef(null);
+	const storeRef = useRef(null);
+
+	const onChange = (e) => {
+		setChecked(e.target.value);
+		handleMethodChange(e);
 	};
 
 	const isChecked = (value) => value === checked;
 
 	return (
 		<SBody1>
-			<InputLabel
-				htmlFor={input.value.home}
-				inputRef={input.homeRef}
-				type={input.type}
-				name={input.name}
-				method={input.method}
-				value={input.value.home}
-				onChange={input.onChange}
-				checked={isChecked('home')}>
-				Home delivery
-			</InputLabel>
-			<InputLabel
-				htmlFor={input.value.store}
-				inputRef={input.storeRef}
-				type={input.type}
-				name={input.name}
-				method={input.method}
-				value={input.value.store}
-				onChange={input.onChange}
-				checked={isChecked('store')}>
-				Collect from store
-			</InputLabel>
+			{body1.map((input) => (
+				<InputLabel
+					key={input.id}
+					htmlFor={input.value}
+					inputRef={input.value === 'home' ? homeRef : storeRef}
+					type={input.type}
+					name={input.name}
+					method={input.method}
+					value={input.value}
+					onChange={onChange}
+					checked={isChecked(input.value)}>
+					{input.value === 'home' ? 'Home delivery' : 'Collect from store'}
+				</InputLabel>
+			))}
 		</SBody1>
 	);
 };
---
+++ src/assets/radioInputs.jsx
@@ -0,0 +1,16 @@
+export const body1 = [
+	{
+		id: 1,
+		type: 'radio',
+		name: 'body1',
+		method: 'method',
+		value: 'home'
+	},
+	{
+		id: 2,
+		type: 'radio',
+		name: 'body1',
+		method: 'method',
+		value: 'store'
+	}
+];
